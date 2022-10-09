7aee00c4d9b1fadca6b7d0d02f1005cd41da2806
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2022-10-09 16:28:07 +0200

    Add extra formatting in "list" command
    Missing sources or target will now be clearly shown.

--- src/bot/command/list.py
+++ src/bot/command/list.py
@@ -30,19 +30,24 @@ class ListCog(Cog):
         await interaction.response.defer()
         target_channel = self._get_target_channel_info()
         source_channels = self._get_source_channels_info()
-        response = f"Target: {target_channel}\n\nSources:\n{source_channels}"
+        response = f"{target_channel}\n\n{source_channels}"
         await interaction.send(response)
 
     def _get_source_channels_info(self) -> str:
         channel_ids = self._source_channels.get_channel_ids()
+        if not channel_ids:
+            return "Sources: **not set**"
         channels = [self._bot.get_channel(channel_id) for channel_id in channel_ids]
         channel_infos = [f" - {self._format_channel(channel)}" for channel in channels]
-        return "\n".join(channel_infos)
+        channel_info_str = "\n".join(channel_infos)
+        return f"Sources:\n{channel_info_str}"
 
     def _get_target_channel_info(self) -> str:
         channel_id = self._target_channel.get_channel_id()
+        if not channel_id:
+            return "Target: **not set**"
         target_channel = self._bot.get_channel(channel_id)
-        return self._format_channel(target_channel)
+        return f"Target: {self._format_channel(target_channel)}"
 
     def _format_channel(self, channel: GuildChannel | Thread | PrivateChannel) -> str:
         return f"**{channel}** - {channel.guild}"
